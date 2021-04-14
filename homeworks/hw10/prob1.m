75dc03ed8ac03279df77bd99537989caf5615579
Dimitri Lezcano <dlezcan1@jhu.edu>
2021-04-14 01:15:00 -0400

    HW10: added data and empty scripts

---
+++ homeworks/hw10/prob1.m
@@ -0,0 +1,12 @@
+%% prob1.m
+%
+% this is to answer problem 1 in the homework
+%
+% - written by: Dimitri Lezcano
+
+function prob1
+
+
+
+
+end
---
+++ homeworks/hw10/prob2.m
@@ -0,0 +1,12 @@
+%% prob2.m
+%
+% this is to answer problem 2 in the homework
+%
+% - written by: Dimitri Lezcano
+
+function prob2
+
+
+
+
+end
